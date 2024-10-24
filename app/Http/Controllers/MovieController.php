92c8a6c1415deb068f03f7e5bfbfc2c86514a676
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-24 16:04:18 +0200

    recover db data in MovieController index function

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index(){
 
+        // recupero i dati dal DB
+        $movies = Movie::all();
         return view('movies.index');
 
     }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,4 @@
 @endsection
 
 {{--
-
-5 - Create un model Movie php artisan make:model Movie
-6 - Create un controller che gestirà la rotta php artisan make:controller MovieController
 7 - All’interno della funzione index() del controller, recuperate tutti i film dal database e passateli alla view, che quindi li visualizzerà a schermo, tramite delle card. --}}
