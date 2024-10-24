2939e1d65f6bcca3e7471e932620cace277ebf97
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-24 15:51:33 +0200

    added route to movies.index

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title', 'Homepage')
+
+@section('main-content')
+
+    <h1>movies index</h1>
+
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,10 +8,8 @@
 
 @endsection
 
-{{-- 1 - Create un nuovo progetto Laravel 10
-2 - Tramite phpMyAdmin create un nuovo database laravel_model_controller
-3 - Importate nel vostro database la tabella movies in allegato
-4 - Inserite le vostre credenziali per il database nel file .env
+{{--
+
 5 - Create un model Movie php artisan make:model Movie
 6 - Create un controller che gestirà la rotta php artisan make:controller MovieController
 7 - All’interno della funzione index() del controller, recuperate tutti i film dal database e passateli alla view, che quindi li visualizzerà a schermo, tramite delle card. --}}
